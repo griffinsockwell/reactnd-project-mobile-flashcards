d43240d1951fc56c3ad09abcaaa04480778fbb4e
Griffin Sockwell <griffinsockwell@users.noreply.github.com>
2017-10-24 17:47:42 -0400

    Change storage key in DecksAPI

--- src/api/DecksAPI.js
+++ src/api/DecksAPI.js
@@ -1,6 +1,6 @@
 import { AsyncStorage } from 'react-native';
 
-const STORAGE_KEY = 'UdaciFlashcards';
+const STORAGE_KEY = 'UdaciFlashcards:decks';
 
 export const getDecks = () => AsyncStorage.getItem(STORAGE_KEY);
 
